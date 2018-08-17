d6bbda6df3ab71f3019537864201cafbecc917be
mconners6052 <mconners6052@floridapoly.edu>
2018-08-17 08:36:39 -0400

    course progress

---
+++ Gothic Horror CYOA/Assets/State.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(14, 12)][SerializeField] string storyText;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+}
